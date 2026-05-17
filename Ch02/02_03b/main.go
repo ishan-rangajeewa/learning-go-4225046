79b1d33850bef32a0e2a1eba86f1b726eb785783
Ishan Rangajeewa <119867116+ishan-rangajeewa@users.noreply.github.com>
2026-05-17 01:35:01 +0000

    bufio conver str to float64

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,4 +13,13 @@ func main() {
 	fmt.Print("Enter number: ")
 	str, _ := reader.ReadString('\n')
 	println(str)
+
+	fmt.Print("Enter number: ")
+	str, _ = reader.ReadString('\n')
+	aFloat,err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+	if err != nil {
+		fmt.Println(err)
+	}else{
+		fmt.Printf("Value is: %v and type is: %T\n",aFloat,aFloat)
+	}
 }
