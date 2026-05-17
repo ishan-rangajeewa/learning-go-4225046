2ee0c3179ed66d4f1dc3a8c1ab5236419c9c5314
Ishan Rangajeewa <119867116+ishan-rangajeewa@users.noreply.github.com>
2026-05-17 01:48:48 +0000

    arithmatic opearation

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+	i1, i2, i3 := 12, 13, 14
+	intsum := i1 + i2 + i3
+	var f1, f2, f3 float64 = 12.5, 13.5, 14.5
+	floatsum := f1 + f2 + f3
+
+	answer := float64(i1) + f1
+	fmt.Printf("Sum of f %v, %v and %v is: %v\n", f1, f2, f3, floatsum)
+	fmt.Printf("Sum of i %v, %v and %v is: %v\n", i1, i2, i3, intsum)
+	fmt.Printf("Sum of i+f %v and %v is: %v\n", i1, f1, answer)
+	
 }
