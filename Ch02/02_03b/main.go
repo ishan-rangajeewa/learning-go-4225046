f403b5e349b19c78e9ac31970ac07d4270cf933d
Ishan Rangajeewa <119867116+ishan-rangajeewa@users.noreply.github.com>
2026-05-17 01:18:02 +0000

    Read the consolwe write String value

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter number: ")
+	str, _ := reader.ReadString('\n')
+	println(str)
 }
