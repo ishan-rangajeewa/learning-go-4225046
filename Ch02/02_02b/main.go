d73721b14e224a2597a494deea804e992d750ce5
Ishan Rangajeewa <119867116+ishan-rangajeewa@users.noreply.github.com>
2026-05-17 01:03:57 +0000

    Fist task

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,16 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	stringLength, err := fmt.Println("Value is:", aNumber)
+	if err == nil {
+		fmt.Println("Length: ", stringLength)
+	}
+	fmt.Printf("Value is fPrint %v\n", aNumber)
+	fmt.Printf("Value type : %T\n", aNumber)
 }
