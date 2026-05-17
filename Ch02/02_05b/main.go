d9fbe5ef90129f32f3d86debda534285b97af3e2
Ishan Rangajeewa <119867116+ishan-rangajeewa@users.noreply.github.com>
2026-05-17 10:11:55 +0000

    math function

--- Ch02/02_05b/main.go
+++ Ch02/02_05b/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -9,5 +10,14 @@ func main() {
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	sum := f1 + f2 + f3
 	fmt.Println("Float sum:", sum)
+
+	sum = math.Round(sum*100)/100
+
+	fmt.Printf("Rounded float sum: %v\n", sum)
+
+	fmt.Println("Pie: ",math.Pi)
+	var radius int = 5
+	area := math.Pi * math.Pow(float64(radius), 2)
+	fmt.Printf("Area of circle with radius %d: %.2f\n", radius, area)
 
 }
